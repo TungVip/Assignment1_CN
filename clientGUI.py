5b9ca409e780a5de9f8656a0a062a611c9c551d1
PeterTung <nthtung03@gmail.com>
2023-12-02 23:34:48 +0700

    first commit

--- clientGUI.py
+++ clientGUI.py
@@ -1,9 +1,8 @@
 import os
-import tkinter as tk
-from tkinter import filedialog, scrolledtext, ttk
+import PySimpleGUI as sg
+import threading
 
 from client import FileClient
-
 
 class FileClientGUI:
     def __init__(self):
@@ -11,142 +10,63 @@ class FileClientGUI:
         self.path = None
         self.files = None
 
-        self.root = tk.Tk()
-        self.root.title("P2P Client GUI")
+        self.layout = [
+            [sg.Button("Connect", key="connect")],
+            [sg.Text("Hostname:"), sg.InputText(key="hostname"), sg.Button("Submit", key="submit_hostname")],
+            [sg.Text("Choose Path:"), sg.InputText(key="path"), sg.FolderBrowse(key="browse_path")],
+            [sg.Multiline(size=(60, 20), key="logs", disabled=True)],
+            [sg.Multiline(size=(60, 20), key="repo", disabled=True)],
+            [sg.Text("Publish:"), sg.InputText(key="file_path", readonly=True), sg.FileBrowse(key="browse_file"),
+             sg.InputText(key="file_name"), sg.Button("Publish", key="publish")],
+            [sg.Text("Fetch:"), sg.InputText(key="fetch_file"), sg.Button("Fetch", key="fetch")],
+            [sg.Button("Quit", key="quit")]
+        ]
 
-        # Hostname Frame
-        self.hostname_frame = ttk.Frame(self.root)
-        self.hostname_frame.pack_forget()
+        self.window = sg.Window("P2P Client GUI", self.layout)
 
-        hostname_label = ttk.Label(self.hostname_frame, text="Hostname:")
-        hostname_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        while True:
+            event, values = self.window.read()
 
-        self.hostname_entry = ttk.Entry(self.hostname_frame)
-        self.hostname_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-
-        init_hostname_button = ttk.Button(
-            self.hostname_frame, text="Submit", command=self.init_hostname
-        )
-        init_hostname_button.grid(row=0, column=2, padx=5, pady=5, sticky=tk.W)
-
-        # Path Frame
-        self.path_frame = ttk.Frame(self.root)
-        self.path_frame.pack(padx=10, pady=10, side=tk.TOP)
-
-        path_label = ttk.Label(self.path_frame, text="Choose Path:")
-        path_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-
-        self.path_button = ttk.Button(
-            self.path_frame, text="Browse", command=self.init_path
-        )
-        self.path_button.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-
-        # Log Box
-        self.main_frame = ttk.Frame(self.root)
-        self.main_frame.pack(padx=10, pady=10, side=tk.BOTTOM)
-
-        self.log_frame = ttk.Frame(self.main_frame)
-        self.log_frame.pack(side=tk.LEFT)
-
-        label_log = ttk.Label(self.log_frame, text="Logs")
-        label_log.pack(pady=10)
-
-        self.log_box = scrolledtext.ScrolledText(
-            self.log_frame, wrap=tk.WORD, width=60, height=20, state=tk.DISABLED
-        )
-        self.log_box.pack(padx=10, pady=10, side=tk.LEFT, expand=True)
-
-        self.repo_frame = ttk.Frame(self.main_frame)
-        self.repo_frame.pack_forget()
-
-        self.label_repo = ttk.Label(self.repo_frame, text="My Repository")
-        self.label_repo.pack(pady=10)
-
-        self.repo_box = scrolledtext.ScrolledText(
-            self.repo_frame,
-            wrap=tk.WORD,
-            width=60,
-            height=20,
-            state=tk.DISABLED,
-            bg="white",
-            fg="black",
-        )
-        self.repo_box.pack(padx=10, pady=10, side=tk.RIGHT, expand=True)
-
-        # Commands Frame (Initially hidden)
-        self.commands_frame = ttk.Frame(self.root)
-        # Use pack for the commands frame
-        self.commands_frame.pack_forget()
-
-        self.create_commands_widgets()
-        self.root.protocol("WM_DELETE_WINDOW", self.quit_client)
-
-    def create_commands_widgets(self):
-        # Publish Section
-        publish_label = ttk.Label(self.commands_frame, text="Publish:")
-        publish_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-
-        self.file_path_entry = ttk.Entry(self.commands_frame, state="readonly")
-        self.file_path_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-
-        browse_button = ttk.Button(
-            self.commands_frame, text="Browse", command=self.browse_file
-        )
-        browse_button.grid(row=0, column=2, padx=5, pady=5, sticky=tk.W)
-
-        # New Entry for the second argument (file name)
-        self.file_name_entry = ttk.Entry(self.commands_frame)
-        self.file_name_entry.grid(row=0, column=3, padx=5, pady=5, sticky=tk.W)
-
-        publish_button = ttk.Button(
-            self.commands_frame, text="Publish", command=self.publish
-        )
-        publish_button.grid(row=0, column=4, padx=5, pady=5, sticky=tk.W)
-
-        # Fetch Section
-        fetch_label = ttk.Label(self.commands_frame, text="Fetch:")
-        fetch_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-
-        self.fetch_entry = ttk.Entry(self.commands_frame)
-        self.fetch_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
-
-        fetch_button = ttk.Button(self.commands_frame, text="Fetch", command=self.fetch)
-        fetch_button.grid(row=1, column=2, padx=5, pady=5, sticky=tk.W)
-
-        # Quit Button
-        quit_button = ttk.Button(
-            self.commands_frame, text="Quit", command=self.quit_client
-        )
-        quit_button.grid(row=2, column=0, columnspan=5, pady=10)
+            if event == sg.WINDOW_CLOSED or event == "quit":
+                self.quit_client()
+                break
+            elif event == "connect":
+                self.connect_to_server()
+            elif event == "submit_hostname":
+                self.init_hostname(values["hostname"])
+            elif event == "browse_path":
+                self.init_path()
+            elif event == "browse_file":
+                self.browse_file()
+            elif event == "publish":
+                self.publish(values["file_path"], values["file_name"])
+            elif event == "fetch":
+                self.fetch(values["fetch_file"])
 
     def init_path(self):
-        path = filedialog.askdirectory()
-        if path != "":
+        path = sg.popup_get_folder("Choose Path")
+        if path and os.path.exists(path):
             self.client.path = path
-            self.path_frame.pack_forget()
-            self.files = {
-                file: False
-                for file in os.listdir(self.client.path)
-                if os.path.isfile(os.path.join(self.client.path, file))
-            }
-            self.hostname_frame.pack(padx=10, pady=10, side=tk.TOP)
+            self.window["path"].update(path)
+            self.files = {file: False for file in os.listdir(self.client.path) if os.path.isfile(os.path.join(self.client.path, file))}
+            self.window["hostname"].update(disabled=False)
 
-    def init_hostname(self):
-        hostname = self.hostname_entry.get()
+    def init_hostname(self, hostname):
         if hostname:
             try:
                 client_address = self.client.login(hostname)
                 self.log(f"Client address: {client_address}")
                 if client_address:
                     self.log(f"Hostname '{hostname}' set successfully.")
-                    # threading.Thread(target=self.client.start,
-                    # args=(client_address,)).start()
-                    self.client.start(client_address)
-                    # Show the main commands frame
-                    self.hostname_frame.pack_forget()
-                    self.commands_frame.pack(side=tk.BOTTOM, expand=True)
-                    self.main_frame.pack(side=tk.TOP)
-                    self.repo_frame.pack(side=tk.RIGHT)
+                    threading.Thread(target=self.client.start, args=(client_address,)).start()
+                    self.window["hostname"].update(disabled=True)
+                    self.window["repo"].update(f"Current directory: {self.client.path}\n")
+                    self.window["submit_hostname"].update(disabled=True)
+                    self.window["browse_path"].update(disabled=True)
+                    self.window["file_path"].update(readonly=False)
+                    self.window["file_name"].update(disabled=False)
+                    self.window["publish"].update(disabled=False)
+                    self.window["fetch_file"].update(disabled=False)
                     self.process_file(first_time=True)
             except Exception as e:
                 self.log(f"Error setting hostname: {e}")
@@ -154,109 +74,71 @@ class FileClientGUI:
             self.log("Hostname cannot be empty.")
 
     def browse_file(self):
-        file_path = filedialog.askopenfilename(initialdir=self.client.path)
-        if file_path != "":
+        file_path = sg.popup_get_file("Choose File", initial_folder=self.client.path)
+        if file_path and os.path.exists(file_path):
             if os.path.dirname(file_path) == self.client.path:
-                self.file_path_entry.config(state="normal")
-                self.file_path_entry.delete(0, tk.END)
-                self.file_path_entry.insert(0, file_path)
-                self.file_path_entry.config(state="disabled")
+                self.window["file_path"].update(readonly=False)
+                self.window["file_path"].update(file_path)
+                self.window["file_path"].update(readonly=True)
             else:
                 self.log(f"Choose file in the {self.client.path} directory!")
 
-    def publish(self):
-        file_path = self.file_path_entry.get()
-        local_name = file_path.split("/")[-1]  # Extracting the file name
-        file_name = self.file_name_entry.get()  # Get the second argument (file name)
+    def publish(self, file_path, file_name):
+        local_name = file_path.split("/")[-1]
         if not file_name or not local_name:
             self.log("Error publishing file: Please fill in the blank!")
             return
         try:
-            publish_status = self.client.publish(
-                self.client.client_socket, local_name, file_name
-            )
+            publish_status = self.client.publish(self.client.client_socket, local_name, file_name)
             if publish_status:
-                self.file_path_entry.config(state=tk.NORMAL)
-                self.file_path_entry.delete(0, tk.END)
-                self.file_path_entry.config(state=tk.DISABLED)
-                self.file_name_entry.delete(0, tk.END)
+                self.window["file_path"].update(readonly=False)
+                self.window["file_path"].update("")
+                self.window["file_path"].update(readonly=True)
+                self.window["file_name"].update("")
         except Exception as e:
             self.log(f"Error publishing file: {e}")
 
-    def fetch(self):
-        file_name = self.fetch_entry.get()
+    def fetch(self, file_name):
         if not file_name:
             self.log("Error fetching file: File name cannot be blank!")
             return
         try:
             self.client.fetch(self.client.client_socket, file_name)
-            self.fetch_entry.delete(0, tk.END)
+            self.window["fetch_file"].update("")
         except Exception as e:
             self.log(f"Error fetching file: {e}")
 
     def quit_client(self):
         self.client.quit(self.client.client_socket)
-        self.root.destroy()
+        self.window.close()
 
     def log(self, message):
-        self.log_box.config(state=tk.NORMAL)
-        self.log_box.insert(tk.END, message + "\n")
-        self.log_box.see(tk.END)
-        self.log_box.config(state=tk.DISABLED)
+        self.window["logs"].update(disabled=False)
+        self.window["logs"].print(message, end="\n")
+        self.window["logs"].update(disabled=True)
 
     def process_file(self, first_time=False):
         if self.client.path is not None:
-            new_files = [
-                file
-                for file in list(self.client.local_files.keys())
-                if self.files[file] is False
-            ]
-            dir_unchange = list(self.files.keys()) == [
-                file
-                for file in os.listdir(self.client.path)
-                if os.path.isfile(os.path.join(self.client.path, file))
-            ]
-            publish_unchange = (
-                len(list(self.client.local_files.keys())) == 0 or len(new_files) == 0
-            )
+            new_files = [file for file in list(self.client.local_files.keys()) if self.files[file] is False]
+            dir_unchange = list(self.files.keys()) == [file for file in os.listdir(self.client.path) if os.path.isfile(os.path.join(self.client.path, file))]
+            publish_unchange = len(list(self.client.local_files.keys())) == 0 or len(new_files) == 0
 
             if first_time is True or dir_unchange is False or publish_unchange is False:
-                self.repo_box.config(state=tk.NORMAL)
-                self.repo_box.delete("1.0", tk.END)
-                self.files = {
-                    file: False
-                    for file in os.listdir(self.client.path)
-                    if os.path.isfile(os.path.join(self.client.path, file))
-                }
-
-                self.repo_box.insert(
-                    tk.END, f"Current directory: {self.client.path}\r\n"
-                )
+                self.window["repo"].update(disabled=False)
+                self.window["repo"].update("Current directory: {self.client.path}\n")
+                self.files = {file: False for file in os.listdir(self.client.path) if os.path.isfile(os.path.join(self.client.path, file))}
 
                 all_files = list(self.files.keys())
                 all_files.sort()
                 for file in all_files:
-                    if (
-                        file in self.client.local_files
-                        and self.client.local_files[file] is not None
-                    ):
-                        self.repo_box.insert(
-                            tk.END,
-                            f"\n{file[:25] + '...' if len(file) > 25 else file} - "
-                            f"{self.client.local_files[file]}",
-                        )
+                    if file in self.client.local_files and self.client.local_files[file] is not None:
+                        self.window["repo"].update(f"\n{file[:25] + '...' if len(file) > 25 else file} - {self.client.local_files[file]}", append=True)
                         self.files[file] = True
                     else:
-                        self.repo_box.insert(
-                            tk.END,
-                            f"\n{file[:35] + '...' if len(file) > 35 else file} "
-                            "(not published)",
-                        )
-                self.repo_box.config(state=tk.DISABLED)
+                        self.window["repo"].update(f"\n{file[:35] + '...' if len(file) > 35 else file} (not published)", append=True)
+                self.window["repo"].update(disabled=True)
 
-        self.root.after(5000, self.process_file)
-
+        sg.popup_timed("Processing Files...", auto_close_duration=5000, non_blocking=True)
 
 if __name__ == "__main__":
     gui = FileClientGUI()
-    gui.root.mainloop()
--- serverGUI.py
+++ serverGUI.py
@@ -6,7 +6,7 @@ from server import ServerLogic
 
 class ServerGUI:
     def __init__(self, host, port):
-        self.server = FileServer(
+        self.server = ServerLogic(
             host,
             port,
             log_callback=self.log_message,
@@ -15,53 +15,51 @@ class ServerGUI:
 
         # Layout
         layout = [
-            [sg.Text("Logs", font=("Helvetica", 24, "bold"))],
-            [sg.Multiline("", key="-LOG-", size=(80, 10), disabled=True, autoscroll=True)],
-            [sg.Text("Requests", font=("Helvetica", 24, "bold"))],
-            [sg.Multiline("", key="-REQUEST_LOG-", size=(60, 10), font=("Helvetica", 14), disabled=True, autoscroll=True)],
-            [sg.InputText(key="-COMMAND-", size=(60, 1), font=("Helvetica", 12)),
-             sg.Button("Send Command", key="-SEND_COMMAND-")],
-            [sg.Button("Start Server", key="-START_SERVER-"),
-             sg.Button("Stop Server", key="-STOP_SERVER-")],
+            [sg.Text('Logs', font=('Helvetica', 24, 'bold'))],
+            [sg.Multiline("", size=(60, 10), key='-LOG-', autoscroll=True,  reroute_cprint=True, font=('Helvetica', 12))],
+            [sg.Text('Requests', font=('Helvetica', 24, 'bold'))],
+            [sg.Multiline(size=(60, 10), key='-REQUEST_LOG-', disabled=True, autoscroll=True, font=('Helvetica', 12))],
+            [sg.InputText(key='-COMMAND-', size=(40, 1), font=('Helvetica', 12)), sg.Button('Send Command', key='-SEND_COMMAND-', font=('Helvetica', 10))],
+            [sg.Button('Start Server', key='-START_SERVER-', font=('Helvetica', 10)),
+             sg.Button('Stop Server', key='-STOP_SERVER-', font=('Helvetica', 10))]
         ]
 
-        self.window = sg.Window("P2P Server GUI", layout, finalize=True)
-
+        self.window = sg.Window('P2P Server GUI', layout, finalize=True)
+        
     def start_server(self):
+        self.log_message("Starting server...")
         self.server.start()
 
     def stop_server(self):
+        self.log_message("Server stopped.\n")
         self.server.shutdown()
 
     def send_command(self, command):
         threading.Thread(target=self.server.process_server_command, args=(command,)).start()
 
-    def log_message(self, message):
-        self.window["-LOG-"].update(value=message + "\n")
-
-    def log_request(self, message):
-        self.window["-REQUEST_LOG-"].update(value=message + "\n")
-
-    def run(self):
-        while True:
-            event, values = self.window.read()
-
-            if event == sg.WIN_CLOSED:
-                self.server.shutdown()
-                break
-
-            elif event == "-START_SERVER-":
-                self.start_server()
-
-            elif event == "-STOP_SERVER-":
-                self.stop_server()
-
-            elif event == "-SEND_COMMAND-":
-                command = values["-COMMAND-"]
-                self.send_command(command)
-
+    def on_close(self):
+        self.server.shutdown()
         self.window.close()
 
-if __name__ == "__main__":
-    gui = ServerGUI("localhost", 8888)
-    gui.run()
+    def log_message(self, message):
+        self.window["-LOG-"].print(message, end="\n")
+
+    def log_request(self, message):
+        self.window["-REQUEST_LOG-"].print(message, end="\n")
+
+if __name__ == '__main__':
+    gui = ServerGUI("localhost", 50000)
+
+    while True:
+        event, values = gui.window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            gui.on_close()
+            break
+        elif event == '-START_SERVER-':
+            gui.start_server()
+        elif event == '-STOP_SERVER-':
+            gui.stop_server()
+        elif event == '-SEND_COMMAND-':
+            gui.send_command()
+
